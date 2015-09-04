ca6dce2551e01b0060eac42e1342a84dae7b2c39
rbaertsch <baertsch@soe.ucsc.edu>
2015-09-04 11:48:52 -0700

    Clean up.

--- webapp/client/d3_boxplot.js
+++ webapp/client/d3_boxplot.js
@@ -58,6 +58,8 @@ function processStrata(strata, strataSampleSets, $div) {
 }
 
 BoxPlotChartData = function(pivotData, exclusions) {
+    debugger
+
     var h = pivotData.getRowKeys();
     var value_color_scale = d3.scale.category10();
     var rowCategoricalVariables = [];
