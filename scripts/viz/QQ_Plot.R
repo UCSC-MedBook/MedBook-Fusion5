14f7d5fe37e565779828ca859637cb26a280e7b7
tedgoldstein <tedgoldstein@gmail.com>
2016-01-03 14:59:40 -0800

    QQ Plot for two variables. Not sure its meaningful.

--- scripts/viz/QQ_Plot.R
+++ scripts/viz/QQ_Plot.R
@@ -9,11 +9,28 @@ outputdata       = args[4];
 
 svg(outputviz);
 
-input = read.delim(infilename, header=TRUE, sep="\t", row.names=1, na.strings="N/A", stringsAsFactors=TRUE);
-input = na.omit(input)
-data = unlist(input[1]);
+infile = read.delim(infilename, header=TRUE, sep="\t", row.names=1, na.strings="N/A", stringsAsFactors=TRUE);
+n_infile = dim(infile)[1];
 
-qqnorm(data)
-qqline(data)
+input = na.omit(infile)
+input1 = unlist(input[1]);
+n_input = length(input1);
+
+n_cols = dim(input)[2];
+
+
+cn = colnames(input)
+
+cat(n_input, "of", n_infile , "have useful values");
+
+if (n_cols == 1) {
+    data = unlist(input[1]);
+    qqnorm(data)
+    qqline(data)
+} else {
+    qqplot(unlist(input[1]), unlist(input[2]), xlab=cn[1], ylab=cn[2])
+    qqline(unlist(input[1]))
+    qqline(unlist(input[2]))
+}
 
 ignore = dev.off();
