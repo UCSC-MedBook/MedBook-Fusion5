44e49d0fbed0bb802fbc9a190243864d3864a333
tedgoldstein <tedgoldstein@gmail.com>
2016-01-15 08:41:20 -0800

    Need this too.

---
+++ webapp/client/buttons.js
@@ -0,0 +1,12 @@
+
+window.postButton = function () { 
+    var copy = Charts.find({_id: TheChartID})
+    delete copy["_id"];
+    delete copy["userId"];
+    copy.posted = true;
+    var postedId = Charts.insert(copy);
+    var url = Meteor.absoluteUrl('display/'+postedId);
+    window.medbookpost = { title: document.title, url: url }
+    $.getScript('/postScript');
+}
+
