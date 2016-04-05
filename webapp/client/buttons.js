1c91745cb23779ab971e06dfc8b7154e9dd7a950
tedgoldstein <tedgoldstein@gmail.com>
2016-04-04 23:12:25 -0700

    Print Button

--- webapp/client/buttons.js
+++ webapp/client/buttons.js
@@ -10,3 +10,19 @@ window.postButton = function () {
     $.getScript('/postScript');
 }
 
+window.printButton = function () { 
+    var container = $('.pvtRendererArea').children()
+    var width = container.width();
+    var height = container.height();
+    var printWindow = window.open('', 'PrintMap', 'width=' + width + ',height=' + height);
+    printWindow.document.writeln('<style> body, h1, h2, h3, ol, ul, div { width: auto; border: 0; margin: 0 5%; paddin: 0; float: none;position: static; overflow: visible; }</style>');
+
+    printWindow.document.writeln($(container).html());
+    printWindow.document.close();
+    /*
+    printWindow.print();
+    printWindow.close();
+    */
+};
+
+
